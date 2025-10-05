ce1e0d3bade72ce662eec897eb404e94561312c4
Geoffrey <geoffreyraditya12@gmail.com>
2025-10-05 16:02:14 +0700

    Commit No.3 - Deleting an Item from RecyclerView by Swiping

--- app/src/main/java/com/example/lab_week_06/CatAdapter.kt
+++ app/src/main/java/com/example/lab_week_06/CatAdapter.kt
@@ -2,16 +2,20 @@ package com.example.lab_week_06
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lab_week_06.model.CatModel
 
 class CatAdapter(
     private val layoutInflater: LayoutInflater,
     private val imageLoader: ImageLoader,
-    private val onClickListener: OnClickListener // <- parameter ini sekarang valid
+    private val onClickListener: OnClickListener
 ) : RecyclerView.Adapter<CatViewHolder>() {
 
     private val cats = mutableListOf<CatModel>()
+
+    // ✅ Inisialisasi swipe callback di luar interface
+    val swipeToDeleteCallback = SwipeToDeleteCallback()
 
     fun setData(newCats: List<CatModel>) {
         cats.clear()
@@ -19,9 +23,15 @@ class CatAdapter(
         notifyDataSetChanged()
     }
 
+    fun removeItem(position: Int) {
+        if (position in cats.indices) {
+            cats.removeAt(position)
+            notifyItemRemoved(position)
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
         val view = layoutInflater.inflate(R.layout.item_list, parent, false)
-        // Sekarang tipe `onClickListener` akan cocok
         return CatViewHolder(view, imageLoader, onClickListener)
     }
 
@@ -31,8 +41,42 @@ class CatAdapter(
         holder.bindData(cats[position])
     }
 
-    // PINDAHKAN INTERFACE KE SINI
+    // ✅ Interface listener
     interface OnClickListener {
         fun onItemClick(cat: CatModel)
+    }
+
+    // ✅ Kelas callback swipe di luar interface
+    inner class SwipeToDeleteCallback : ItemTouchHelper.SimpleCallback(
+        0,
+        ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+    ) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ) = false
+
+        override fun getMovementFlags(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder
+        ): Int {
+            return if (viewHolder is CatViewHolder) {
+                makeMovementFlags(
+                    ItemTouchHelper.ACTION_STATE_IDLE,
+                    ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+                ) or makeMovementFlags(
+                    ItemTouchHelper.ACTION_STATE_SWIPE,
+                    ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+                )
+            } else {
+                0
+            }
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val position = viewHolder.adapterPosition
+            removeItem(position)
+        }
     }
 }
--- app/src/main/java/com/example/lab_week_06/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -1,78 +1,84 @@
 package com.example.lab_week_06
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.lab_week_06.CatAdapter
-import com.example.lab_week_06.GlideImageLoader
-import com.example.lab_week_06.R
 import com.example.lab_week_06.model.CatBreed
 import com.example.lab_week_06.model.CatModel
 import com.example.lab_week_06.model.Gender
-import androidx.appcompat.app.AlertDialog
-
-
 
 class MainActivity : AppCompatActivity() {
+
     private val recyclerView: RecyclerView by lazy {
         findViewById(R.id.recycler_view)
     }
+
     private val catAdapter by lazy {
-//Glide is used here to load the images
-//Here we are passing the onClickListener function to the Adapter
-        CatAdapter(layoutInflater, GlideImageLoader(this), object:
-            CatAdapter.OnClickListener {
-            //When this is triggered, the pop up dialog will be shown
+        CatAdapter(layoutInflater, GlideImageLoader(this), object : CatAdapter.OnClickListener {
             override fun onItemClick(cat: CatModel) = showSelectionDialog(cat)
         })
     }
+
+    private val swipeToDeleteCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ) = false
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val position = viewHolder.adapterPosition
+            // ✅ gunakan fungsi yang benar
+            catAdapter.removeItem(position)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//Setup the adapter for the recycler view
+
         recyclerView.adapter = catAdapter
-//Setup the layout manager for the recycler view
-//A layout manager is used to set the structure of the item views
-//For this tutorial, we're using the vertical linear structure
-        recyclerView.layoutManager = LinearLayoutManager(this,
-            LinearLayoutManager.VERTICAL, false)
-//Add data to the model list in the adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+
         catAdapter.setData(
             listOf(
                 CatModel(
-                    Gender.Male,
-                    CatBreed.BalineseJavanese,
-                    "Fred",
-                    "Silent and deadly",
-                    "https://cdn2.thecatapi.com/images/7dj.jpg"
+                    name = "Fred",
+                    breed = CatBreed.BalineseJavanese,
+                    gender = Gender.Male,
+                    biography = "Silent and deadly",
+                    imageUrl = "https://cdn2.thecatapi.com/images/7dj.jpg"
                 ),
                 CatModel(
-                    Gender.Female,
-                    CatBreed.ExoticShorthair,
-                    "Wilma",
-                    "Cuddly assassin",
-                    "https://cdn2.thecatapi.com/images/egv.jpg"
+                    name = "Wilma",
+                    breed = CatBreed.ExoticShorthair,
+                    gender = Gender.Female,
+                    biography = "Cuddly assassin",
+                    imageUrl = "https://cdn2.thecatapi.com/images/egv.jpg"
                 ),
                 CatModel(
-                    Gender.Unknown,
-                    CatBreed.AmericanCurl,
-                    "Curious George",
-                    "Award winning investigator",
-                    "https://cdn2.thecatapi.com/images/bar.jpg"
+                    name = "Curious George",
+                    breed = CatBreed.AmericanCurl,
+                    gender = Gender.Unknown,
+                    biography = "Award winning investigator",
+                    imageUrl = "https://cdn2.thecatapi.com/images/bar.jpg"
                 )
             )
         )
     }
-//This will create a pop up dialog when one of the items from the recycler view
+
+    // ✅ Pindahkan ke dalam class MainActivity
     private fun showSelectionDialog(cat: CatModel) {
         AlertDialog.Builder(this)
-//Set the title for the dialog
             .setTitle("Cat Selected")
-//Set the message for the dialog
             .setMessage("You have selected cat ${cat.name}")
-//Set if the OK button should be enabled
-            .setPositiveButton("OK") { _, _ -> }.show()
+            .setPositiveButton("OK", null)
+            .show()
     }
 }
